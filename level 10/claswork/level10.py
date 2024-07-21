6a8002bcfc0bf7d6d498e205c0b64bf77eb4dc32
nikolozi16 <n.cindeliani13@gmail.com>
2024-07-21 18:23:32 +0400

    commit

--- level 10/claswork/level10.py
+++
@@ -1,6 +0,0 @@
-num1 = user_password = input("please enter password: ")
-
-if num1 == "nika16":
-      print("სწორია")
-else:
-      print("არასწორია")      
--- level 10/homework/levell10.py
+++
@@ -1,6 +0,0 @@
-num=0
-
-for num in range(0, 100):
-
-  print(num)
-  
